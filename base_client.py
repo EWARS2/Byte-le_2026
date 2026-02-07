f7adf3a93733bef36c770e3232071ba3f0f6974b
EWARS2 <jefmacho99@gmail.com>
2026-02-07 13:39:21 -0600

    logs should be excluded but aren't lol

--- base_client.py
+++ base_client.py
@@ -1,13 +1,19 @@
+
 from game.client.user_client import UserClient
 from game.common.avatar import Avatar
 from game.common.enums import ObjectType, ActionType
 from game.common.map.game_board import GameBoard
 from game.constants import *
 
+# Custom imports
+import math
+
+
 class Client(UserClient):
 
     def __init__(self):
         super().__init__()
+        self.called = 0
 
     def team_name(self) -> str:
         """
@@ -23,5 +29,23 @@ class Client(UserClient):
         :param actions:     This is the actions object that you will add effort allocations or decrees to.
         :param world:       Generic world information
         """
-        return []
 
+        self.called += 1
+
+        action1 = ActionType.MOVE_DOWN
+        action2 = ActionType.MOVE_RIGHT
+
+        # Get position of player
+        #position = Avatar.position.
+        #print(position)
+
+        # Get position of goal
+
+        # Check surroundings
+
+
+
+
+
+
+        return [action1, action2]
